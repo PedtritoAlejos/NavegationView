fa6b69f145eb33e00c2217dd1479dc566764d969
pedroalejosb <pedroalejosb@gmail.com>
2021-07-31 12:33:31 -0400

    Initial Commit

---
+++ Tarea-03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tarea-03
+//
+//  Created by pealejosb on 31-07-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tarea-03/Tarea_03App.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea_03App.swift
+//  Tarea-03
+//
+//  Created by pealejosb on 31-07-21.
+//
+
+import SwiftUI
+
+@main
+struct Tarea_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
