e0efde86dc717756c760c248535e2a9e75a1955a
pedroalejosb <pedroalejosb@gmail.com>
2021-08-02 12:52:29 -0400

    Se implementa NavegationView

---
+++ Tarea-03/RowViewRestauranteView.swift
@@ -0,0 +1,20 @@
+//
+//  RowViewRestauranteView.swift
+//  Tarea-03
+//
+//  Created by pealejosb on 01-08-21.
+//
+
+import SwiftUI
+
+struct RowViewRestauranteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RowViewRestauranteView_Previews: PreviewProvider {
+    static var previews: some View {
+        RowViewRestauranteView()
+    }
+}
---
+++ Tarea-03/datos.swift
@@ -0,0 +1,8 @@
+//
+//  datos.swift
+//  Tarea-03
+//
+//  Created by pealejosb on 01-08-21.
+//
+
+import Foundation
